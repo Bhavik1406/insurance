bd854b8b5e414fadde43bb767dec3c23eeb23a30
Jaydip <jaydip@designli.co>
2018-02-13 13:57:14 +0530

    updated

---
+++ src/test/java/Login_Page/LoginScreenTest.java
@@ -0,0 +1,24 @@
+package Login_Page;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+
+
+
+public class LoginScreenTest {
+
+
+	WebDriver driver;
+
+	@Test
+	public void setup(){
+
+		System.out.println("***************************");
+		System.out.println("This is Jenkins Project.");
+		System.out.println("***************************");
+	}
+}	
+
+
+
+
